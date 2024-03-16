069fab5a10e273bd50b8c207db30d59d62568137
Kdavid013 <gradon91519@gmail.com>
2024-03-16 15:11:58 +0100

    Version 1.1.2 saját adatok lekérés és megjelenítése

--- app/src/main/java/com/example/testdetection/RegisztracioFragment.java
+++ app/src/main/java/com/example/testdetection/RegisztracioFragment.java
@@ -19,7 +19,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
@@ -28,9 +27,6 @@ import com.android.volley.toolbox.Volley;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 public class RegisztracioFragment extends Fragment {
 
@@ -45,9 +41,7 @@ public class RegisztracioFragment extends Fragment {
     private static Tag tag;
     private static final String TAG = "MyActivity";
 
-    private static String tagId = "";
-
-    private static final String dbAddress = "http://192.168.1.147/regisztracio.php?ID=";
+    public static String tagId = "";
 
 
     EditText felhasznaloNevET;
@@ -55,76 +49,30 @@ public class RegisztracioFragment extends Fragment {
 
     DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        /**nfc adapter mely felelős az nfc eléhetőségének lekérésére*/
-        nfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
-        if (nfcAdapter == null) {
-            Toast.makeText(getActivity(), "NFC is not available", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        /**létrehozok egy intentet melyből ki fogom olvasni az nfc tipusát*/
-        pendingIntent = PendingIntent.getActivity(getActivity(), 0, new Intent(getActivity(), getClass()).addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT), PendingIntent.FLAG_MUTABLE);
-        //pendingIntent
-
-        /**filter mely alapján kiválasztom milyen típusú nfc kártyákat olvassak be*/
-        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
-        intentFiltersArray = new IntentFilter[]{tech};
-        techListsArray = new String[][]{new String[]{NfcF.class.getName()}};
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_regisztracio,null);
 
-
-
         regisztracioButton = root.findViewById(R.id.regisztracioGomb);
         nfcIdTextView = root.findViewById(R.id.unipass_kartya_azonosito_adat);
         felhasznaloNevET = root.findViewById(R.id.regisztracio_felhasznalo_nev_adat);
 
+        nfcIdTextView.setText(MainActivity.tagId);
+
         regisztracioButton.setOnClickListener(v -> {
-            insertToDb();
-            csatlakozasIdeje = df.format(new Date());
-            Log.d(TAG, csatlakozasIdeje);
+
+            if (!(tagId.equals("") || felhasznaloNevET.getText().toString().equals(""))) {
+                insertToDb();
+                nfcIdTextView.setText("");
+                felhasznaloNevET.setText("");
+            }
+            else {
+                Toast.makeText(getActivity(),"Hiányzó információ! Ellenőrizze a megadott adatokat!",Toast.LENGTH_LONG).show();
+            }
         });
         return root;
-    }
-
-    public void onPause() {
-        super.onPause();
-        nfcAdapter.disableForegroundDispatch(getActivity());
-        Log.d(TAG, "im on pause");
-    }
-
-    public void onResume() {
-        super.onResume();
-        nfcAdapter.enableForegroundDispatch(getActivity(), pendingIntent, intentFiltersArray, techListsArray);
-        Log.d(TAG, "i'm on resume");
-        readFromIntent(getActivity().getIntent());
-        nfcIdTextView.setText(tagId);
-    }
-
-
-    /**
-     * Az intentből kiszedi az adott NFC tag id-ját
-     *
-     * @param intent az a cselekvés amiből ki tudjuk szedni a NFC azonositó adatait
-     */
-    private void readFromIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-            tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            tagId = getHex(tag.getId());
-
-            //Log.d(TAG,tagId);
-        }
     }
 
     /**
@@ -134,46 +82,25 @@ public class RegisztracioFragment extends Fragment {
      */
 
     public void insertToDb() {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, dbAddress + tagId + "&fn=" + felhasznaloNevET.getText() + "&csi=" + df.format(new Date()) , response -> {
-            //logToDebug();
-            Log.d(TAG, response);
-            if (response.equals("success")) {
-                Toast.makeText(getActivity(), "sikeres regisztráció", Toast.LENGTH_SHORT).show();
-            } else if (response.equals("failure")) {
-
-                Toast.makeText(getActivity(), "sikertelen", Toast.LENGTH_SHORT).show();            }
-        }, error -> {
-            Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show();     }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET,MainActivity.regisztracioDbCim + MainActivity.tagId + "&fn=" + felhasznaloNevET.getText() + "&csi=" + df.format(new Date()), response -> {
                 //logToDebug();
-                Map<String, String> data = new HashMap<>();
-                data.put("NFC_id", tagId);
-                Log.d(TAG, data.toString());
-                return data;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-        requestQueue.add(stringRequest);
+                Log.d(TAG, response);
+                if (response.equals("success")) {
+                    Toast.makeText(getActivity(), "sikeres regisztráció", Toast.LENGTH_SHORT).show();
+                } else if (response.equals("failure")) {
 
-        Log.d(TAG, stringRequest.toString() + requestQueue);
+                    Toast.makeText(getActivity(), "sikertelen", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(getActivity(), "A kártya már regisztrálva van", Toast.LENGTH_SHORT).show();
+                }
+            }, error -> {
+                Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }) {
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+            requestQueue.add(stringRequest);
+
     }
-    /**
-     * Át alakítja a kapott bytes tömböb egy stringé
-     *
-     * @param bytes byte tömb ami az NFC tag id-ját tartalmazza
-     */
-    private String getHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; i++) {
-            int b = bytes[i] & 0xff;
-            if (b < 0x10) sb.append('0');
-            sb.append(Integer.toHexString(b));
-            if (i >= 0) {
-                sb.append("");
-            }
-        }
-        //Log.d(TAG,sb.toString());
-        return sb.toString().toUpperCase();
-    }
+
 }
